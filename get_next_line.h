924476fc3e0a93bf408f070573a931ce832176e0
Mustapha AMDAH <muamdah@e1r5p22.42.fr>
2018-02-15 15:24:14 +0100

    ajout

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muamdah <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/15 14:59:36 by muamdah           #+#    #+#             */
+/*   Updated: 2018/02/15 15:03:11 by muamdah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUF_SIZE
+
+# include "libft.h"
+
+int		get_next_line(const int fd, char **line);
+#endif
