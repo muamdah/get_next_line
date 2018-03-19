5c2bf3ee407adc9bd2c78e46208b3c955d44c967
Mustapha AMDAH <muamdah@e1r3p21.42.fr>
2018-03-19 15:46:52 +0100

    ajout des commentaires

--- get_next_line.c
+++ get_next_line.c
@@ -18,31 +18,36 @@ int		get_next_line(const int fd, char **line)
 	char		*tmp;
 	char		buf[BUFF_SIZE + 1];
 	int			ret;
-/* ---[ 1 ]----------Parse de toute les erreurs possibles -------------------------------------*/
+/*==============================================================================================*\																				             |
+|        [ 1 ]                   Parse de toute les erreurs possibles                            |
+\*==============================================================================================*/
 	if (fd < 0 || read(fd, "", 0) < 0 || line == NULL || fd > OPEN_MAX)
 		return (-1);
-/* ---[ 2 ]----------initialiser la STATIC pour le 1er passage --------------------------------*/
+/* ---------------------------initialiser la STATIC pour le 1er passage -----------------------*/
 	if (!str[fd])
 		str[fd] = ft_strnew(1);
-/* ---[ 3 ]----------Lecture du fichier jusqu'a un '\n' + Verif que RET n'est pas null --------*/
+/*===============================================================================================*\																								  |
+|        [ 2 ]     Lecture du fichier jusqu'a un '\n' + Verif que RET n'est pas null              |
+\*===============================================================================================*/
 	while (!(ft_strchr(str[fd], '\n')) && (ret = read(fd, buf, BUFF_SIZE)))
 	{
-/* ---[ . ]----------insertion du BUFF dans tmp reporté dans la STATIC ------------------------*/
+/* ------------------insertion du BUFF dans tmp reporté dans la STATIC ------------------------*/
 		buf[ret] = '\0';
 		tmp = ft_strjoin(str[fd], buf);
-/* ---[  ]-----------LEAKS = écrase les fuites memoires ---------------------------------------*/
+/* ---------------------------LEAKS = écrase les fuites memoires ------------------------------*/
 		ft_strdel(&str[fd]);
 		str[fd] = tmp;
 	}
-/* ---[ 4 ]----------Verif que la static existe -----------------------------------------------*/
+/*===============================================================================================*\																								  |
+|        [ 3 ]                     Insertion de la phrase dans *line                              |
+\*===============================================================================================*/
 	if (ft_strlen(str[fd]) == 0)
 		return (0);
-/* ---[ 5 ]----------insertion de la phrase dans *line ----------------------------------------*/
 	*line = ft_strsub(str[fd], 0, ft_strnlen(str[fd], '\n'));
-/* ---[ . ]----------Recupération du reste apres '\n' dans tmp reporté dans la STATIC ---------*/
+/* ----------Recupération du reste apres '\n' dans tmp reporté dans la STATIC -----------------*/
 	tmp = ft_strsub(str[fd], ft_strnlen(str[fd], '\n') + 1,
 			ft_strlen(&str[fd][ft_strnlen(str[fd], '\n')]));
-/* ---[  ]-----------LEAKS = écrase les fuites memoires ---------------------------------------*/
+/* ---------------------------LEAKS = écrase les fuites memoires ------------------------------*/
 	ft_strdel(&str[fd]);
 	str[fd] = tmp;
 	return (1);
--- get_next_line.h
+++ get_next_line.h
@@ -13,7 +13,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1
+# define BUFF_SIZE 1000000
 # include "libft/libft.h"
 # include <unistd.h>
 # include <fcntl.h>
